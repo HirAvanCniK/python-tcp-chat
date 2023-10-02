4e3d2982ed6d93cccc37d50b6f696c3a26a0e75b
HirAvanCniK <vanniandrea244466666@gmail.com>
2023-10-02 02:55:08 +0200

    Update listener.py

--- listener.py
+++ listener.py
@@ -1,30 +1,30 @@
 import socket, sys, threading, os
 
 def handle_client(client_socket):
-    # Richiede il nome del client
-    client_socket.send(b'Inserisci il tuo nome: ')
+    # Requires client name
+    client_socket.send(b'Insert your name: ')
     client_name = client_socket.recv(1024).decode().strip()
 
-    # Messaggio di benvenuto
-    welcome_message = f'Benvenuto, {client_name}!\n'
+    # Welcome message
+    welcome_message = f'Welcome, {client_name}!\n'
     client_socket.send(welcome_message.encode())
 
     while True:
         try:
-            # Ricevi il messaggio inviato dal client
+            # Receive the message sent by the client
             data = client_socket.recv(1024).decode()
             if not data:
                 break
 
-            # Stampa il messaggio con il nome del client
+            # Prints the message with the client name
             message_with_name = f'({client_name}) {data}'
             print(message_with_name)
 
-            # Manda il messaggio a tutti i client tranne al mittente
+            # Send the message to all clients except the sender
             broadcast_message(message_with_name, client_socket)
 
         except Exception as e:
-            print(f"Errore nella gestione del client: {e}")
+            print(f"Error in client management: {e}")
             break
 
 def broadcast_message(message, client_to_exclude):
@@ -33,40 +33,40 @@ def broadcast_message(message, client_to_exclude):
             try:
                 client.send(message.encode())
             except Exception as e:
-                print(f"Errore nell'invio del messaggio a un client: {e}")
+                print(f"Error sending message to client: {e}")
                 client.close()
                 clients.remove(client)
 
-HOST = '0.0.0.0'  # Ascolta su tutte le interfacce
+HOST = '0.0.0.0'  # Listen on all interfaces
 try:
     PORT = int(sys.argv[1])
 except:
     print(f"Usage: python3 {os.path.basename(__file__)} <int:port>")
     exit()
 
-# Lista per tutti i client che si connetteranno
+# List for all clients that will connect
 clients = []
 
 # Server socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
-server.listen(20)
+server.listen(20) # Max clients
 
-print(f"In ascolto su {socket.gethostbyname(socket.gethostname())}:{PORT}")
+print(f"Listen on {socket.gethostbyname(socket.gethostname())}:{PORT}")
 
 while True:
     try:
-        # Accetta la connessione di un client
+        # Accept a client connection
         client_sock, client_addr = server.accept()
-        print(f"Connessione accettata da {client_addr[0]}:{client_addr[1]}")
+        print(f"Connection accepted {client_addr[0]}:{client_addr[1]}")
 
-        # Aggiungi il client alla lista
+        # Add the client to the list
         clients.append(client_sock)
 
-        # Thread per gestire il client
+        # Thread to manage the client
         client_handler = threading.Thread(target=handle_client, args=(client_sock,))
         client_handler.start()
 
     except KeyboardInterrupt:
-        print("Chiusura del server.")
+        print("Server shutdown...")
         break

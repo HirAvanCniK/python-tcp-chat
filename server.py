cfee48aa6cf1bbf3c24b80fbbf3048892ee1921e
HirAvanCniK <vanniandrea244466666@gmail.com>
2024-04-10 20:54:20 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
-import socket, sys, threading, os
+import socket, sys, threading, os, datetime
 # from pwn import log
+
+def logs(message):
+    time = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    open("server.log", "a").write(f"[{time}] {message.strip()}\n")
 
 def handle_client(client_socket):
     # Requires client name
@@ -25,7 +29,8 @@ def handle_client(client_socket):
             welcome_message = f'Welcome, {client_name}!\n'
             client_socket.send(welcome_message.encode())
             # log.warn(f"User logged in as {client_name}")
-            print(f"User logged in as {client_name}")
+            # print(f"User logged in as {client_name}")
+            logs(f"User logged in as {client_name}")
             usernames[client_socket] = client_name
 
             # Add the client to the list
@@ -41,14 +46,16 @@ def handle_client(client_socket):
                     # Prints the message with the client name
                     message_with_name = f'({client_name}) {data}'
                     # log.info(message_with_name)
-                    print(message_with_name)
+                    # print(message_with_name)
+                    logs(message_with_name)
 
                     # Send the message to all clients except the sender
                     broadcast_message(message_with_name, client_socket)
                 except Exception as e:
                     # log.critical(f"Error in client management: {e}")
                     # log.warning(f"Client {client_name} has disconnected")
-                    print(f"Client {client_name} has disconnected")
+                    # print(f"Client {client_name} has disconnected")
+                    logs(f"Client {client_name} has disconnected")
                     clients.remove(client_socket)
                     usernames.pop(client_socket)
                     broadcast_message(f"{client_name} has disconnected\n", client_socket)
@@ -61,14 +68,16 @@ def broadcast_message(message, client_to_exclude):
                 client.send(message.encode())
             except Exception as e:
                 # log.warning(f"Error sending message to client: {e}")
-                print(f"Error sending message to client: {e}")
+                # print(f"Error sending message to client: {e}")
+                logs(f"Error sending message to client: {e}")
                 client.close()
                 clients.remove(client)
                 usernames.pop(client)
 
 SERVER_KEY = os.urandom(32)
 # log.warn(f"Server random key: {SERVER_KEY.hex()}")
-print(f"Server random key: {SERVER_KEY.hex()}")
+# print(f"Server random key: {SERVER_KEY.hex()}")
+logs(f"Server random key: {SERVER_KEY.hex()}")
 open("SERVER_KEY", "w").write(SERVER_KEY.hex())
 
 try:
@@ -87,14 +96,16 @@ server.bind(('0.0.0.0', PORT))
 server.listen()
 
 # log.info(f"Listen on {socket.gethostbyname(socket.gethostname())}:{PORT}")
-print(f"Listen on {socket.gethostbyname(socket.gethostname())}:{PORT}")
+# print(f"Listen on {socket.gethostbyname(socket.gethostname())}:{PORT}")
+logs(f"Listen on {socket.gethostbyname(socket.gethostname())}:{PORT}")
 
 while True:
     try:
         # Accept a client connection
         client_sock, client_addr = server.accept()
         # log.success(f"Connection accepted {client_addr[0]}:{client_addr[1]}")
-        print(f"Connection accepted {client_addr[0]}:{client_addr[1]}")
+        # print(f"Connection accepted {client_addr[0]}:{client_addr[1]}")
+        logs(f"Connection accepted {client_addr[0]}:{client_addr[1]}")
 
         # Thread to manage the client
         client_handler = threading.Thread(target=handle_client, args=(client_sock,))
@@ -102,5 +113,6 @@ while True:
     except:
         # log.info("Server shutdown...")
         # print("Server shutdown...")
+        # logs("Server shutdown...")
         # break
         pass
